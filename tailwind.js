5cc888c2e2a9f7e729364ba41ecf8d87cf0050fd
Suzon Ali <suzonali@Suzons-MacBook-Pro.local>
2024-02-18 14:11:22 +0600

    dadfa

---
+++ tailwind.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            primary:    "#030712",
+            success:    "#1DD100",
+            info:       "#FFBF0F",
+            warning:    "#F78C9C",
+            low_white:     "rgba(3, 7, 18, 0.60)",
+            
+        }
+      }
+    }
+  }
